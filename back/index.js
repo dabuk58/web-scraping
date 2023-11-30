845e9981e79e1d2b8b085282c469e2b56823efbe
dabuk58 <jakub.deska@simpleformat.pl>
2023-11-30 12:09:24 +0100

    ckolwiek

--- back/index.js
+++ back/index.js
@@ -17,8 +17,8 @@ app.use(cors(corsOptions));
 
 puppeteer.use(stealthPlugin());
 
-const search = async (from, to, departureDate, departureTime) => {
-    const browser = await puppeteer.launch({ headless: true });
+const searchPortalPasazera = async (from, to, departureDate, departureTime) => {
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage()
 
     await page.goto('https://portalpasazera.pl/');
@@ -49,18 +49,76 @@ const search = async (from, to, departureDate, departureTime) => {
     return results;
 }
 
+const searchRozkladJazdyPKP = async (from, to, departureDate, departureTime) => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage()
+
+    await page.goto('https://rozklad-pkp.pl/');
+
+    await page.click('.css-47sehv');
+    await page.type('#from-station', from);
+    await page.type('#to-station', to);
+  
+    await page.evaluate((departureDate, departureTime) => {
+      const day1 = document.querySelector('input[name="date"]');
+      const day2 = document.querySelector('input[name="dateStart"]');
+      const day3 = document.querySelector('input[name="dateEnd"]');
+      const day4 = document.querySelector('input[name="REQ0JourneyDate"]');
+      day1.value = departureDate;
+      day2.value = departureDate;
+      day3.value = departureDate;
+      day4.value = departureDate;
+  
+      const time1 = document.querySelector('input[name="time"]');
+      const time2 = document.querySelector('input[name="REQ0JourneyTime"]');
+      time1.value = departureTime;
+      time2.value = departureTime;
+    }, departureDate, departureTime);
+  
+    await page.click('#singlebutton');
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+  
+    const results = await page.evaluate(() => Array.from(document.querySelector('tbody').querySelectorAll('tr'), (e) => ({
+    //   from: e.children[1].children[0].innerText,
+    //   to: e.children[1].children[1].innerText,
+      dateFrom: e.children[2].innerText.slice(0, 8),
+      fromTime: e.children[3].children[0].children[0].children[2].innerText,
+      toTime: e.children[3].children[1].children[0].children[2].innerText,
+      dateTo: e.children[2].innerText.slice(0, 8)
+    //   totalTime: e.children[4].innerText,
+    //   interchanges: e.children[5].innerText,
+    })));
+
+    await browser.close();
+
+    console.log(results);
+
+    return results;
+}
+
+const search = async (from, to, departureDate, departureTime, source) => {
+
+    switch(source) {
+        case 'rozkladJazdyPKP':
+            return searchRozkladJazdyPKP(from, to, departureDate, departureTime);
+        case 'portalPasazera':
+            return searchPortalPasazera(from, to, departureDate, departureTime); 
+    }
+}
+
 app.get('/search', async (req, res) => {
     try{
         const from = req.query.from;
         const to = req.query.to;
         const departureDate = req.query.departureDate;
         const departureTime = req.query.departureTime;
+        const source = req.query.source;
 
-        if(!from || !to || !departureDate || !departureTime){
+        if(!from || !to || !departureDate || !departureTime || !source){
             return res.status(400).send('Please provide proper data');
         }
 
-        const results = await search(from, to, departureDate, departureTime);
+        const results = await search(from, to, departureDate, departureTime, source);
 
         res.json(results);
     } catch (error) {
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -31,24 +31,43 @@ export class AppComponent implements OnInit{
         updateOn: 'change',
         validators: Validators.required
       }),
-      date: new FormControl()
+      date: new FormControl(),
+      source: new FormControl(null, {
+        validators: Validators.required
+      })
     })
   }
 
   onSearch(){
     const fromCity: string = this.formGroup.get('from')?.value;
     const toCity: string = this.formGroup.get('to')?.value;
-    const date: string = this.formatDate(this.formGroup.get('date')?.value);
+    const source: string = this.formGroup.get('source')?.value;
+    const date: string = this.formatDate(this.formGroup.get('date')?.value, source);
 
-    const url = `http://localhost:3000/search?from=${fromCity}&to=${toCity}&departureDate=${date}&departureTime=20:00`;
+    const url = `http://localhost:3000/search?from=${fromCity}&to=${toCity}&departureDate=${date}&departureTime=20:00&source=${source}`;
     
     this.connections = this.http.get<connection[]>(url);
   }
 
-  formatDate(date: Date){
-    const day = ('0' + date.getDate()).slice(-2);
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const year = date.getFullYear();
+  formatDate(date: Date, source: string){
+
+    let day: string | number = '';
+    let month: string | number = '';
+    let year: number = 1212;
+
+    switch(source){
+      case 'portalPasazera':
+        day = ('0' + date.getDate()).slice(-2);
+        month = ('0' + (date.getMonth() + 1)).slice(-2);
+        year = date.getFullYear();
+        return `${day}.${month}.${year}`;
+
+      case 'rozkladJazdyPKP':
+        day = date.getDate();
+        month = date.getMonth() + 1; 
+        year = date.getFullYear() % 100; 
+        return `${day}.${month < 10 ? '0' : ''}${month}.${year < 10 ? '0' : ''}${year}`;
+    }
 
     return `${day}.${month}.${year}`;
   }
